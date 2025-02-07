7899b2681d76b2f49f8345f54a20dd68115c8082
伊钱耀 <yiqianyao@bytedance.com>
2025-02-07 17:28:31 +0800

    feat: add follow emitter

--- docs/Emitter.md
+++ docs/Emitter.md
@@ -1,2 +1,9 @@
 # Emitter
+
 粒子发射器
+
+## FollowEmitter
+
+跟随鼠标移动的粒子发射器
+
+<code src="./demos/FollowEmitter.tsx"></code>
--- docs/demos/FollowEmitter.tsx
+++ docs/demos/FollowEmitter.tsx
@@ -2,22 +2,15 @@ import React, { useEffect, useRef } from 'react';
 import {
   Alpha,
   Body,
-  BoxZone,
   Color,
   CrossZone,
-  Debug,
-  Emitter,
   FollowEmitter,
   Force,
-  Gravity,
   Life,
   Mass,
-  MeshRender,
-  Position,
   Proton,
   Radius,
   Rate,
-  Rotate,
   Scale,
   ScreenZone,
   Span,
@@ -28,17 +21,10 @@ import {
 import * as THREE from 'three';
 import {
   AmbientLight,
-  BoxGeometry,
-  Mesh,
-  MeshPhongMaterial,
-  PlaneGeometry,
   PointLight,
   Scene,
-  SphereGeometry,
   WebGLRenderer,
 } from 'three';
-// @ts-ignore
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { createSprite } from './utils';
 
 export default () => {
@@ -60,7 +46,6 @@ class SceneManager {
   renderer: WebGLRenderer;
   scene: Scene;
   camera: THREE.PerspectiveCamera;
-  control: OrbitControls;
   proton!: Proton;
 
   constructor(wrap: HTMLDivElement) {
@@ -76,9 +61,7 @@ class SceneManager {
     );
     camera.position.z = 500;
     this.camera = camera;
-    const control = new OrbitControls(camera, wrap);
-    this.control = control;
-    control.update();
+
 
     // 创建渲染器
     const renderer = new THREE.WebGLRenderer({
@@ -90,9 +73,10 @@ class SceneManager {
     wrap.appendChild(renderer.domElement);
 
     this.initLights(scene);
-    this.initPlane(scene);
+
 
     const proton = new Proton();
+    this.proton = proton;
     const emitter = new FollowEmitter();
     emitter.rate = new Rate(new Span(5, 7), new Span(0.01, 0.02));
     emitter.addInitialize(new Mass(1));
@@ -117,32 +101,11 @@ class SceneManager {
   }
 
   animate = () => {
-    const { renderer, scene, camera, control, proton } = this;
+    const { renderer, scene, camera, proton } = this;
     proton.update();
     renderer.render(scene, camera);
-    control.update();
     requestAnimationFrame(this.animate);
   };
-
-  createEmitter(obj: any) {
-    const emitter = new Emitter();
-    emitter.rate = new Rate(new Span(5, 10), new Span(0.1, 0.25));
-    emitter.addInitialize(new Mass(1));
-    emitter.addInitialize(new Radius(10));
-    emitter.addInitialize(new Life(2, 4));
-    emitter.addInitialize(new Body(obj.Body));
-    emitter.addInitialize(new Position(new BoxZone(100)));
-    emitter.addInitialize(new Velocity(200, new Vector3D(0, 1, 1), 30));
-
-    emitter.addBehaviour(new Rotate('random', 'random'));
-    emitter.addBehaviour(new Scale(1, 0.1));
-    // Gravity
-    emitter.addBehaviour(new Gravity(3));
-    emitter.p.x = obj.p.x;
-    emitter.p.y = obj.p.y;
-    emitter.emit();
-    return emitter;
-  }
 
   initLights(scene: Scene) {
     const ambientLight = new AmbientLight(0xffffff, 0.3);
@@ -151,34 +114,5 @@ class SceneManager {
     const pointLight = new PointLight(0xffffff, 10000, 100000);
     pointLight.position.set(0, 1, 0);
     scene.add(pointLight);
-  }
-
-  initPlane(scene: Scene) {
-    const groundGeo = new PlaneGeometry(10000, 10000);
-    const groundMat = new MeshPhongMaterial({
-      color: 0xffffff,
-    });
-    groundMat.color.setHSL(0.095, 1, 0.75);
-
-    const ground = new Mesh(groundGeo, groundMat);
-    ground.rotation.x = -Math.PI / 2;
-    ground.position.y = -200;
-    scene.add(ground);
-  }
-
-  createMesh(geoType: 'sphere' | 'cube') {
-    if (geoType == 'sphere') {
-      const geometry = new SphereGeometry(10, 8, 8);
-      const material = new MeshPhongMaterial({
-        color: '#ff0000',
-      });
-      return new Mesh(geometry, material);
-    } else {
-      const geometry = new BoxGeometry(20, 20, 20);
-      const material = new MeshPhongMaterial({
-        color: '#00ffcc',
-      });
-      return new Mesh(geometry, material);
-    }
   }
 }
